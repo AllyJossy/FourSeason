724d22a13a9486399f66e257c73fe0f8fa9c8245
AllyJossy <ally.fer14@gmail.com>
2017-12-03 16:10:21 -0600

    index2

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
     ini_set("log_errors",'On');      //se activa el archivo de errores y se personaliza
     ini_set("error_log','error/php_errors.log");
 
-    include("../Conexion/Conexion.php"); // Cargamos el archivo con información de la conexión
+    include("Conexion/Conexion.php"); // Cargamos el archivo con información de la conexión
     $conexion = mysqli_connect($host, $hostuser, $hostpw, $hostdb) or die("Problema con la conexión.");
     session_start();
     if(isset($_SESSION["usuario"])) {
         if ($id>0){
-            if($tipo=="Administrador")
-                header("Location: ../Inicio/Admin.html");
-            else{
-                header("Location: ../Inicio/Normal.html");
+            if($tipo=="Administrador") {
+                header("Location: Inicio/Admin.html");
+            }else if ($tipo == "User"){
+                header("Location: Inicio/Normal.html");
+            }else {
+                header("Location: Inicio/Cliente.html");
             }
-        }else{
-            echo "Usuario o contraseña incorrectos <br>";
         }
     }
     if(isset($_POST["register"])) {
@@ -28,8 +28,8 @@
             $password = mysqli_real_escape_string($conexion, $_POST["password"]);
             //$password = sha1($password);
             $password = md5($password);
-            $query = "INSERT INTO Usuarios (usuario, tusuario, password) VALUES('$usuario', '$tusuario', '$password')";
-            if(mysqli_query($conexion, $query)) {
+            $sql = "INSERT INTO Usuarios (id_usuario, usuario, tipo_usuario, password) VALUES('','$usuario', '$tusuario', '$password')";
+            if ($conexion->query($sql) == TRUE) {
                 echo '<script>alert("Registro Completado")</script>';
             }
         }
@@ -50,13 +50,13 @@
             if(mysqli_num_rows($result) > 0) {
                 $_SESSION['usuario'] = $usuario;
                 if ($id>0){
-                    if($tipo=="Administrador")
-                        header("Location: ../Inicio/Admin.html");
-                    else{
-                        header("Location: ../Inicio/Normal.html");
+                    if($tipo=="Administrador") {
+                        header("Location: Inicio/Admin.html");
+                    }else if ($tipo == "User"){
+                        header("Location: Inicio/Normal.html");
+                    }else {
+                        header("Location: Inicio/Cliente.html");
                     }
-                }else{
-                    echo "Usuario o contraseña incorrectos <br>";
                 }
             }
             else {
@@ -73,68 +73,65 @@
         <title>Four Season</title>
         <link rel="shortcut icon" href="http://static02.hongkiat.com/logo/hkdc/favicon.ico">
         <link rel="icon" href="http://static02.hongkiat.com/logo/hkdc/favicon.ico">
-        <link rel="stylesheet" type="text/css" media="all" href="../estilos/estilosFormulario.css">
-        <link rel="stylesheet" type="text/css" media="all" href="../estilos/responsiveFormulario.css">
+        <link rel="stylesheet" type="text/css" media="all" href="estilos/estilosFormulario.css">
+        <link rel="stylesheet" type="text/css" media="all" href="estilos/responsiveFormulario.css">
     </head>
-    <section>
     <!--oncontextmenu="return false" onkeydown="return false-->
     <br/><br/>
-    <section id = "container"></section>
-    <H1> <center>
+    <center>
+        <H1>
             Bienvenido a Four Season
-        </center></H1>
+        </H1>
         <br/>
-        <img src="../Imagenes/Hotel1.jpg"
-             srcset="../Imagenes/Hotel1-320.jpg 320w,
-             ../Imagenes/Hotel1-375.jpg 375w,
-             ../Imagenes/Hotel1-425.jpg 425w,
-             ../Imagenes/Hotel1-768.jpg 768w,
-            ../Imagenes/Hotel1-1024.jpg 1024w,
-            ../Imagenes/Hotel1-1440.jpg 1440w,
-            ../Imagenes/Hotel1-2560.jpg 2560w">
+        <img src="Imagenes/FourSeason.jpg">
+    </center>
         <?php
         if(isset($_GET["action"]) == "login") {
             ?>
-            <h3 align="center">Login</h3>
-            <br/>
-            <form id="hongkiat-form"  method="post">
-                <div id="wrapping" class="clearfix">
-                    <section id="aligned">
-                        <input type="text" name="usuario" id="usuario" placeholder="Usuario" autocomplete="off" tabindex="1" class="form-control">
-                        <input type="text" name="password" id="password" placeholder="Password" autocomplete="off" tabindex="2" class="form-control">
+            <section id="container">
+                <h3 align="center">Login</h3>
+                <br/>
+                <form id="hongkiat-form"  method="post">
+                    <div id="wrapping" class="clearfix">
+                        <section id="aligned">
+                            <input type="text" name="usuario" id="usuario" placeholder="Usuario" autocomplete="off" tabindex="1" class="txtinput">
+                            <input type="text" name="password" id="password" placeholder="Password" autocomplete="off" tabindex="2" class="txtinput">
+                        </section>
+                    </div>
+                    <section id="buttons">
+                        <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Borrar">
+                        <input type="submit" name="login" id="submitbtn" class="submitbtn" tabindex="3" value="Enviar">
+                        <br style="clear:both;">
                     </section>
-                </div>
-                <section id="buttons">
-                    <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Borrar">
-                    <input type="submit" name="login" id="submitbtn" class="submitbtn" tabindex="3" value="Enviar">
-                    <br style="clear:both;">
-                </section>
-                <p align="center"><a href="index.php">Registrar</a></p>
-            </form>
+                </form>
+            </section>
+            <p align="center"><a href="index.php">Registrar</a></p>
             <?php
         }
         else {
             ?>
-            <h3 align="center">Registrar</h3>
-            <br/>
-            <form id="hongkiat-form" method="post">
-                <div id="wrapping" class="clearfix">
-                    <section id="aligned">
-                        <input type="text" name="usuario" id="usuario" placeholder="Usuario" autocomplete="off" tabindex="4" class="form-control">
-                        <input type="text" name="tusuario" id="tusuario" placeholder="TipoUsuario" autocomplete="off" tabindex="5" class="form-control">
-                        <input type="text" name="password" id="password" placeholder="Password" autocomplete="off" tabindex="6" class="form-control">
+            <section id="container">
+                <h3 align="center">Registrar</h3>
+                <br/>
+                <form id="hongkiat-form" method="post">
+                    <div id="wrapping" class="clearfix">
+                        <section id="aligned">
+                            <input type="text" name="usuario" id="usuario" placeholder="Usuario" autocomplete="off" tabindex="4" class="txtinput">
+                            <input type="text" name="tusuario" id="tusuario" placeholder="TipoUsuario" autocomplete="off" tabindex="5" class="txtinput">
+                            <input type="text" name="password" id="password" placeholder="Password" autocomplete="off" tabindex="6" class="txtinput">
+                        </section>
+                    </div>
+                    <section id="buttons">
+                        <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Borrar">
+                        <input type="submit" name="register" id="submitbtn" class="submitbtn" tabindex="7" value="Enviar">
+                        <br style="clear:both;">
                     </section>
-                </div>
-                <section id="buttons">
-                    <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Borrar">
-                    <input type="submit" name="register" id="submitbtn" class="submitbtn" tabindex="7" value="Enviar">
-                    <br style="clear:both;">
-                </section>
-                <p align="center"><a href="index.php?action=login">Login</a></p>
-            </form>
+                </form >
+            </section>
+            <p align="center"><a href="index.php?action=login">Login</a></p>
             <?php
         }
         ?>
-    </section>
+
     </body>
 </html>
